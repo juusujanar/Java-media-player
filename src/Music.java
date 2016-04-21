f19ee7650710ed2c5026c50d7e491964f502fc80
Monkey911 <andreas.baum95@hotmail.com>
2016-04-21 11:18:21 +0300

    proovisin durationit kätte saada

--- src/Music.java
+++ src/Music.java
@@ -1,14 +1,22 @@
+import javafx.scene.media.Media;
+import javafx.util.Duration;
+
+import javax.sound.sampled.AudioFileFormat;
+import javax.sound.sampled.AudioSystem;
+import java.io.File;
+import java.util.Map;
+
 
 public class Music {
 
     private String song_name;
-    private String length;
+    private String duration;
     protected String path;
 
-    Music(String song_name, String length, String path){
+    Music(String song_name, String path) {
         this.song_name = song_name;
-        this.length = length;
         this.path = path;
+        this.duration = getAudioDuration(path);
     }
 
     public String getSong_name() {
@@ -19,11 +27,22 @@ public class Music {
         this.song_name = song_name;
     }
 
-    public String getLength() {
-        return length;
+    public String getDuration() {
+        return duration;
     }
 
     public void setLength(String length) {
-        this.length = length;
+        this.duration = length;
+    }
+
+    private String getAudioDuration(String path) {
+        File file = new File(path);
+        try {
+            AudioFileFormat fileFormat = AudioSystem.getAudioFileFormat(file);
+            Map<?,?> properties = fileFormat.properties();
+            System.out.println(properties);
+            System.out.println(properties.get("duration"));
+        } catch (Exception ex){System.out.println(ex);};
+        return "2";
     }
 }
